272b256f6010eeb24e123955687e1402753e18f9
Mark <97348004+MarkZingrim@users.noreply.github.com>
2024-10-11 19:09:14 +0530

    Update core.c

--- src/core.c
+++ src/core.c
@@ -1781,9 +1781,9 @@ static void add_argv(const char* what) {
 
 static void load_rcfiles()
 {
-    char* p = getenv("BOX64_RCFILE");
-    if(p && FileExist(p, IS_FILE))
-	LoadRCFile(p);
+    char* rcpath = getenv("BOX64_RCFILE");
+    if(rcpath && FileExist(rcpath, IS_FILE))
+	LoadRCFile(rcpath);
     #ifndef TERMUX
     else if(FileExist("/etc/box64.box64rc", IS_FILE))
         LoadRCFile("/etc/box64.box64rc");
@@ -1796,7 +1796,7 @@ static void load_rcfiles()
     else
         LoadRCFile(NULL);   // load default rcfile
 
-    p = getenv("HOME");
+    char* p = getenv("HOME");
     if(p) {
         char tmp[4096];
         strncpy(tmp, p, 4095);
